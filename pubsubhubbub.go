e3b78a490569453ff57194643d47852894d8066b
Surabhi Gupta <surabhig@rssoftware.co.in>
2015-11-23 12:53:11 +0530

    Handled bad requests for incorrect subscription parameters

--- pubsubhubbub.go
+++ pubsubhubbub.go
@@ -1,55 +1,79 @@
 package main
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"flag"
-	"container/list"
+  "fmt"
+  "log"
+  "net/http"
+  "flag"
+  "container/list"
+  "encoding/json"
 )
 
 var topic = make(map[string]*list.List)
 
 func subscribe(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("*******in subscribe**********")
-		req.ParseForm()
-		if req.PostFormValue("mode") == "Subscribe" {
-			if _, ok := topic[req.PostFormValue("topic")]; ok {
-				topic[req.PostFormValue("topic")].PushBack(req.PostFormValue("callback"))
-				fmt.Println("Subscribed...")
-			}
-			}else if req.PostFormValue("mode") == "Unsubscribe"{
-				if val, ok := topic[req.PostFormValue("topic")]; ok {
-					for e := val.Front(); e != nil; e = e.Next() {
-						if e.Value == req.PostFormValue("callback"){
-							fmt.Println("Found the callback url")
-							fmt.Println(e.Value)
-							val.Remove(e)
-						}
-					}
+  fmt.Println("*******in subscribe**********")
+    req.ParseForm()
+    if req.PostFormValue("mode") == "Subscribe" {
+      if _, ok := topic[req.PostFormValue("topic")]; ok {
+        topic[req.PostFormValue("topic")].PushBack(req.PostFormValue("callback"))
+        fmt.Println("Subscribed...")
+        response := "SUBSCRIPTION ACCEPTED"
+        out,_ :=json.MarshalIndent(response, "202", "    ")
+        w.Write([]byte(out))
+
+       }else{
+         response := "BAD REQUEST"
+         out,_ :=json.MarshalIndent(response, "400", "    ")
+         w.Write([]byte(out))
+       }
+      }else if req.PostFormValue("mode") == "Unsubscribe"{
+				flag:= false
+        if val, ok := topic[req.PostFormValue("topic")]; ok {
+          for e := val.Front(); e != nil; e = e.Next() {
+            if e.Value == req.PostFormValue("callback"){
+              fmt.Println("Found the callback url")
+              fmt.Println(e.Value)
+              val.Remove(e)
+							response := "UNSUBSCRIPTION REQUEST ACCEPTED"
+              out,_ :=json.MarshalIndent(response, "200", "    ")
+              w.Write([]byte(out))
+							flag = true
+							break
+            }
+          }
+        }
+        if(flag==false) {
+					response := "BAD REQUEST"
+          out,_ :=json.MarshalIndent(response, "400", "    ")
+          w.Write([]byte(out))
 				}
-			}
-			fmt.Println(topic["ePayments"].Front().Value)
-		}
 
-		func HTTPListener(host string, port int) {
-			addr := fmt.Sprintf("%s:%d", host, port)
-			http.HandleFunc("/subscribe", subscribe)
-			log.Println("[HTTP] Starting HTTP interface on", addr)
+      }else{
+        response := "Bad Request"
+        out,_ :=json.MarshalIndent(response, "400", "    ")
+        w.Write([]byte(out))
+      }
+    }
 
-			err := http.ListenAndServe(":8080", nil)
-			if err != nil {
-				log.Fatal("[HTTP] ERROR:", err)
-			}
-		}
+    func HTTPListener(host string, port int) {
+      addr := fmt.Sprintf("%s:%d", host, port)
+      http.HandleFunc("/subscribe", subscribe)
+      log.Println("[HTTP] Starting HTTP interface on", addr)
 
-		var (
-			host = flag.String("b", "0.0.0.0", "listen on HOST")
-			httpPort = flag.Int("p", 8080, "use PORT for HTTP")
-		)
+      err := http.ListenAndServe(":8080", nil)
+      if err != nil {
+        log.Fatal("[HTTP] ERROR:", err)
+      }
+    }
 
-		func main() {
-			flag.Parse()
-			topic["ePayments"] = list.New()
-			topic["RS"] = list.New()
-			HTTPListener(*host, *httpPort)
-		}
+    var (
+      host = flag.String("b", "0.0.0.0", "listen on HOST")
+      httpPort = flag.Int("p", 8080, "use PORT for HTTP")
+    )
+
+    func main() {
+      flag.Parse()
+      topic["ePayments"] = list.New()
+      topic["RS"] = list.New()
+      HTTPListener(*host, *httpPort)
+    }
